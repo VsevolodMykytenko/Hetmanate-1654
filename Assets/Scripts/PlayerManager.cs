8149a994331d011816fc159baf78dc6853a9c32d
Mykytenko Vsevolod <vsevolod200229@gmail.com>
2023-06-02 16:36:16 +0300

    Initial work with tokens

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using Photon.Pun;
-using UnityEngine;
-
-public class PlayerManager : MonoBehaviour
-{
-    private PhotonView _photonView;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _photonView = GetComponent<PhotonView>();
-        if (_photonView.IsMine)
-        {
-            CreateController();
-        }
-    }
-//???
-    private void CreateController()
-    {
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", ))
-    }
-}
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.IO;
+// using Photon.Pun;
+// using UnityEngine;
+//
+// public class PlayerManager : MonoBehaviour
+// {
+//     private PhotonView _photonView;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         _photonView = GetComponent<PhotonView>();
+//         if (_photonView.IsMine)
+//         {
+//             CreateController();
+//         }
+//     }
+// //???
+//     private void CreateController()
+//     {
+//         PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", ))
+//     }
+// }
